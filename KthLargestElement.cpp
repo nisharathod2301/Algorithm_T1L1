4a6d3d00898e29c122ba67691ea99deca275fba5
Nisha Hitesh Rathod <109743699+nisharathod2301@users.noreply.github.com>
2023-08-25 10:28:18 +0530

    Min Priority queue

---
+++ KthLargestElement.cpp
@@ -0,0 +1,40 @@
+// C++ code for k largest/ smallest elements in an array
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to find k largest array element
+void kLargest(vector<int>& v, int N, int K)
+{
+	// Implementation using
+	// a Priority Queue
+	priority_queue<int, vector<int>, greater<int>>pq;
+	for(int i = 0; i<K; i++){
+	    pq.push(v[i]);
+	}
+	for(int i = K; i < N; i++){
+	    if(pq.top()<v[i]){
+	        pq.pop();
+	        pq.push(v[i]);
+	    }
+	}
+	cout << pq.top();
+}
+
+
+
+// driver program
+int main()
+{
+	// Given array
+	vector<int> arr
+		= { 11, 3, 2, 1, 15, 5, 4, 45, 88, 96, 50, 45 };
+	// Size of array
+	int n = arr.size();
+	int k = 3;
+	cout << k << "rd largest elements are : ";
+	kLargest(arr, n, k);
+// 	cout << k << " smallest elements are : ";
+// 	kSmalest(arr, n, k);
+}
+
+// This code is contributed by Pushpesh Raj.
