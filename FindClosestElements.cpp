a9b0dc0004317e921f2bb198bc9a02b1765755ea
Nisha Hitesh Rathod <109743699+nisharathod2301@users.noreply.github.com>
2023-08-27 21:12:11 +0530

    2ptr || Sliding window|| Binary search
    O(log(n-k)+k)
    sc: O(k)

--- FindClosestElements.cpp
+++ FindClosestElements.cpp
@@ -1,19 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        int l=0, r=arr.size()-k;
-        //finding the window of k closest elements
-        while(l<r){
-            int m = l+(r-l)/2;
-            //it can also be seen as checking x > (arr[m+k]+arr[m])/2
-            /*don't check it with abs() as it prevents handling the edge cases where arr[mid]==arr[mid+k], cause if we have minus values we can also keep index-wise*/ 
-            if(x-arr[m]>arr[m+k]-x){
-                l = m+1;
-            }
-            else{
-                r=m;
+        int l = 0, r = arr.size() - k;
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            if (x - arr[m] > arr[m + k] - x) {
+                l = m + 1;
+            } else {
+                r = m;
             }
         }
-        return vector<int>(arr.begin()+l, arr.begin()+l+k);
+        return vector<int>(arr.begin() + l, arr.begin() + l + k);
     }
 };
+
+int main() {
+    Solution solution;
+
+    vector<int> arr = {1, 3, 5, 7, 8, 10};
+    int k = 3;
+    int x = 6;
+
+    vector<int> closestElements = solution.findClosestElements(arr, k, x);
+
+    cout << "Closest elements to " << x << ": ";
+    for (int num : closestElements) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
